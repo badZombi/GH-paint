090e47e6005349352e3fa91b5a477ad21d86e0e1
John Z Black <john@gnerd.io>
2024-03-21 12:17:53 -0400

    commit after first full test-run works as expected

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+
+https://www.pixilart.com/draw?ref=home-page#
--- doit.py
+++ doit.py
@@ -1,7 +1,9 @@
 import argparse
+import os
 from PIL import Image
 import matplotlib as mpl
 from datetime import datetime, timedelta
+from subprocess import Popen
 
 hex_to_daily_commit_count = {
     "39d353": 20,
@@ -16,12 +18,12 @@ def find_sunday_before_date(year, month, day):
     this_day = check_day(year, month, day)
     if (this_day == 0):
         print("We can start on this day.")
-        return datetime(year, month, day)
+        return datetime(year, month, day).replace(hour=16, minute=20)
     else:
         sub = day - this_day
         newday = datetime(year, month, day) - timedelta(days=this_day)
         print("The previous Sunday was {}.".format(newday.strftime('%Y-%m-%d')))
-        return newday
+        return newday.replace(hour=16, minute=20)
 
 def check_day(year, month, day):
     try:
@@ -62,30 +64,81 @@ def map_image(image, start_date, col):
     if col:
         h = col
     active_day = start_date
-
+    pixels = []
+    pixel = 1
     while x < h:
         y = 0
         print("week of {}".format(active_day.strftime('%m-%d-%Y')))
         while y < v:
             r, g, b, a = image_data[x,y]
             hex = f"{r:02x}{g:02x}{b:02x}"
-            print("We need {} commits on {}".format(hex_to_daily_commit_count[hex], active_day.strftime('%m-%d-%Y')))
+            commits = hex_to_daily_commit_count[hex]
+            print("We need {} commits on {}".format(commits, active_day.strftime('%m-%d-%Y')))
+            
+            pixels.append({
+                "date": active_day,
+                "commits": commits
+            })
+
             y = y + 1
             active_day = active_day + timedelta(days=1)
         x = x + 1
-
+    return pixels
 
 def main():
     args = arguments()
     curr_date = datetime.now()
 
     # print(curr_date.replace(hour=20, minute=0))
-    start_date = find_sunday_before_date(args.year, args.month, args.day)
     col=None
     if args.columns:
         col = args.columns
 
-    map_image(args.image, start_date, col)
+    # map_image(args.image, start_date, col)
+
+    curr_date = datetime.now()
+    # directory = 'repository-' + curr_date.strftime('%Y-%m-%d-%H-%M-%S')
+    repo = args.repo
+    if repo is not None:
+        start = repo.rfind('/') + 1
+        end = repo.rfind('.')
+        directory = repo[start:end]
+        print("Creating new repo in {} dir".format(directory))
+
+    os.mkdir(directory)
+    os.system('cp {} {}/{}'.format(args.image, directory, "src_image"))
+    os.chdir(directory)
+    run(['git', 'init'])
+    start_date = find_sunday_before_date(args.year, args.month, args.day)
+
+    pixels = map_image("src_image", start_date, col)
+    # print(pixels)
+    for p in pixels:
+        # print(p)
+        this_commit = 0
+        while this_commit < p['commits']:
+            date = p['date'].replace(hour=16, minute=20)
+            commit(date + timedelta(minutes=this_commit))
+            this_commit = this_commit + 1
+    run(['git', 'remote', 'add', 'origin', repo])
+    run(['git', 'push', '-u', 'origin', 'main'])
+
+def commit(date):
+    filename = "{}.py".format(date.strftime('%m%d%Y'))
+    with open(os.path.join(os.getcwd(), filename), 'a') as file:
+        file.write(code(date) + '\n\n')
+    run(['git', 'add', '.'])
+    run(['git', 'commit', '-m', '"%s"' % code(date),
+         '--date', date.strftime('"%Y-%m-%d %H:%M:%S"')])
+
+def code(date):
+    return "print('yep' if this_is_art else 'meh') # {}".format(date.strftime('%H:%M:%S'))
+
+def comment(date):
+    return "pixel data for {}".format(date.strftime('%H:%M:%S'))
+
+def run(commands):
+    Popen(commands).wait()
 
 def arguments():
     parser = argparse.ArgumentParser()
@@ -94,16 +147,19 @@ def arguments():
                         help="""the image to be drawn""")
     parser.add_argument('-m', '--month',
                         required=True, type=int,
-                        help="""theimage to be drawn""")
+                        help="""month of start date""")
     parser.add_argument('-d', '--day',
                         required=True, type=int,
-                        help="""theimage to be drawn""")
+                        help="""day of start date""")
     parser.add_argument('-y', '--year',
                         required=True, type=int,
-                        help="""theimage to be drawn""")
+                        help="""year of start date""")
     parser.add_argument('-c', '--columns',
                         required=False, type=int,
-                        help="""max number of rows to process""")
+                        help="""max number of columns to process""")
+    parser.add_argument('-r', '--repo',
+                        required=True, type=str,
+                        help="""the github repository to push to""")
     return parser.parse_args()
                         
 
